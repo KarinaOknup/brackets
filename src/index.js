3fa8cc229736bbe16ba6f507f39ff773fe5db4d4
KarinaOknup <64832614+KarinaOknup@users.noreply.github.com>
2020-10-22 22:54:10 +0300

    add: test solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let oStr = str.replace(new RegExp(bracketsConfig.map(bc => bc.join('').replace(/([\[\]\(\)\{\}\|])/g, '\\$1')).join('|'), 'g'),'');
+  return (!str) ? true : (str === oStr) ? false : check(oStr, bracketsConfig);// test solution
 }
